b29bac141ec3ff54dc8d40f2b7c1777e83f9f562
CorinneSpruit <corinnemeijdam@hotmail.com>
2020-10-02 14:14:47 +0200

    De kaarten zijn gedefinieerd en geschut.

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,2 +1,77 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class BlackJack {
+
+    public static void main(String[] arg) {
+
+        ArrayList<String> kaarten = new ArrayList<String>();
+
+       kaarten.add("S-Aas");
+       kaarten.add("S-2");
+       kaarten.add("S-3");
+       kaarten.add("S-4");
+       kaarten.add("S-5");
+       kaarten.add("S-6");
+       kaarten.add("S-7");
+       kaarten.add("S-8");
+       kaarten.add("S-9");
+       kaarten.add("S-10");
+       kaarten.add("S-Boer");
+       kaarten.add("S-Dame");
+       kaarten.add("S-Heer");
+       kaarten.add("H-Aas");
+       kaarten.add("H-2");
+       kaarten.add("H-3");
+       kaarten.add("H-4");
+       kaarten.add("H-5");
+       kaarten.add("H-6");
+       kaarten.add("H-7");
+       kaarten.add("H-8");
+       kaarten.add("H-9");
+       kaarten.add("H-10");
+       kaarten.add("H-Boer");
+       kaarten.add("H-Dame");
+       kaarten.add("H-Heer");
+       kaarten.add("K-Aas");
+       kaarten.add("K-2");
+       kaarten.add("K-3");
+       kaarten.add("K-4");
+       kaarten.add("K-5");
+       kaarten.add("K-6");
+       kaarten.add("K-7");
+       kaarten.add("K-8");
+       kaarten.add("K-9");
+       kaarten.add("K-10");
+       kaarten.add("K-Boer");
+       kaarten.add("K-Dame");
+       kaarten.add("K-Heer");
+       kaarten.add("R-Aas");
+       kaarten.add("R-2");
+       kaarten.add("R-3");
+       kaarten.add("R-4");
+       kaarten.add("R-5");
+       kaarten.add("R-6");
+       kaarten.add("R-7");
+       kaarten.add("R-8");
+       kaarten.add("R-9");
+       kaarten.add("R-10");
+       kaarten.add("R-Boer");
+       kaarten.add("R-Dame");
+       kaarten.add("R-Heer");
+
+       Collections.shuffle(kaarten);
+
+       var i = kaarten;
+
+        System.out.println((i));
+
+
+
+
+    }
+
+
+
 }
